f66d994dffef4a960e72b18fa6045896147d22bd
rumieloawer <naluvaiko2004@gmail.com>
2024-11-19 21:32:37 +0200

    Add lab6

---
+++ lab6/lab6.sql
@@ -0,0 +1,9 @@
+SELECT cities.name AS city_name, regions.name AS region_name
+FROM cities
+JOIN regions ON cities.region = regions.uuid
+WHERE cities.population > 350000;
+
+SELECT cities.name AS city_name, regions.name AS region_name
+FROM cities
+JOIN regions ON cities.region = regions.uuid
+WHERE regions.name = 'Nord';
